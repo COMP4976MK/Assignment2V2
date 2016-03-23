5a5aee6dda21d8c19108b858929b40371f4eb647
Kpatena <kamilpatena1221@gmail.com>
2016-03-23 15:50:39 -0700

    API now linked to OptionsWebsite .mdf file

--- DiplomaDataModel/CourseOption/CourseOptionContext.cs
+++ DiplomaDataModel/CourseOption/CourseOptionContext.cs
@@ -4,7 +4,9 @@ namespace DiplomaDataModel.CourseOption
 {
     public class CourseOptionContext : DbContext
     {
-        public CourseOptionContext() : base("DefaultConnection") { }
+        public CourseOptionContext() : base("DefaultConnection") {
+            base.Configuration.ProxyCreationEnabled = false;
+        }
         public DbSet<Option> Options { get; set; }
         public DbSet<YearTerm> YearTerms { get; set; }
         public DbSet<Choice> Choices { get; set; }
