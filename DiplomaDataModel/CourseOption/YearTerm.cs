329b4c827c600410af14843c1b34bf0ada898281
Kpatena <kamilpatena1221@gmail.com>
2016-03-22 17:55:42 -0700

    Project Commit

---
+++ DiplomaDataModel/CourseOption/YearTerm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiplomaDataModel.CourseOption
+{
+    public class YearTerm
+    {
+        
+        [Key]
+        public int YearTermId { get; set; }
+
+        public int Year { get; set; }
+
+        public int Term { get; set; }
+
+        [Display(Name = "Is Default: ")]
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ DiplomaOptions/Controllers/RolesController.cs
@@ -0,0 +1,150 @@
+﻿using DiplomaOptions.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiplomaOptions.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesController : Controller
+    {
+
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult ManageUserRoles()
+        {
+            // prepopulat roles for the view dropdown
+            var listOfRoles = context.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+                new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            var listOfUsers = context.Users.OrderBy(u => u.UserName).ToList().Select(uu => 
+                new SelectListItem { Value = uu.UserName, Text = uu.UserName }).ToList();
+
+            ViewBag.Users = listOfUsers;
+            ViewBag.Roles = listOfRoles;
+            return View();
+        }
+
+        // POST: Roles/Delete/RoleName
+        [HttpPost]
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            context.Roles.Remove(thisRole);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            userManager.AddToRole(user.Id, RoleName);
+
+            ViewBag.ResultMessage = "Role created successfully!";
+
+            // prepopulat roles for the view dropdown
+            var listOfRoles = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            var listOfUsers = context.Users.OrderBy(u => u.UserName).ToList().Select(uu => new SelectListItem { Value = uu.UserName, Text = uu.UserName }).ToList();
+
+            ViewBag.Roles = listOfRoles;
+            ViewBag.Users = listOfUsers;
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            if (userManager.IsInRole(user.Id, RoleName))
+            {
+                if (user.UserName == "A00111111" && RoleName == "Admin")
+                {
+                    ViewData["error"] = "Can't delete this user from Admin";
+
+                } else
+                {
+                    userManager.RemoveFromRole(user.Id, RoleName);
+                    ViewBag.ResultMessage = "Role removed from this user successfully!";
+                }
+            }
+            else
+            {
+                ViewData["error"] = "This user doesn't belong to selected role.";
+            }
+            // prepopulat roles for the view dropdown
+            var listOfRoles = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            var listOfUsers = context.Users.OrderBy(u => u.UserName).ToList().Select(uu => new SelectListItem { Value = uu.UserName, Text = uu.UserName }).ToList();
+            ViewBag.Roles = listOfRoles;
+            ViewBag.Users = listOfUsers;
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            ViewBag.RolesForThisUser = userManager.GetRoles(user.Id);
+
+            // prepopulat roles for the view dropdown
+            var listOfRoles = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            var listOfUsers = context.Users.OrderBy(u => u.UserName).ToList().Select(uu => new SelectListItem { Value = uu.UserName, Text = uu.UserName }).ToList();
+            ViewBag.Roles = listOfRoles;
+            ViewBag.Users = listOfUsers;
+
+            return View("ManageUserRoles");
+        }
+    }
+}
